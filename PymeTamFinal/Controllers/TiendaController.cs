5efe323c9e34f7bfe69038bd2b7dc9f74645431e
Alan Torres <temp_peri@hotmail.com>
2015-09-28 23:44:48 -0500

    Baaa

--- PymeTamFinal/Controllers/TiendaController.cs
+++ PymeTamFinal/Controllers/TiendaController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PymeTamFinal.Contratos.Repositorio;
+using PymeTamFinal.Modelos.ModelosDominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,11 @@ namespace PymeTamFinal.Controllers
 {
     public class TiendaController : Controller
     {
+        IRepositorioBase<Producto> _producto;
+        public TiendaController(IRepositorioBase<Producto>_producto)
+        {
+            this._producto = _producto;
+        }
         // Inicio de la tienda
         public ActionResult Index()
         {
@@ -17,8 +24,9 @@ namespace PymeTamFinal.Controllers
             return View();
         }
         public ActionResult Productos(int? idCategoria, int? pagina, string orden, string busquedaString, string busqueda, int? min, int? max) {
+            var productos = _producto.Cargar(a => a.stock > 0 || a.mostrarSinStock == true && a.habilitado == true).OrderByDescending(a => a.fechaCreacion);
 
-            return View();
+            return View(productos);
         }
         public ActionResult Ayuda() {
             return View();
