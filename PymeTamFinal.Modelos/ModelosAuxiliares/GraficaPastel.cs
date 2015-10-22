86ecc3603726ff352bf28ebc3bea54e0bf63c276
Alan Torres <temp_peri@hotmail.com>
2015-10-21 21:59:19 -0500

    Graficas

---
+++ PymeTamFinal.Modelos/ModelosAuxiliares/GraficaPastel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PymeTamFinal.Modelos.ModelosAuxiliares
+{
+    public class HighCharts : IGrafica
+    {
+        public string plotBackgroundColor { get; set; }
+        public string plotBorderWidth { get; set; }
+    }
+}
---
+++ PymeTamFinal.Modelos/ModelosAuxiliares/GraficaVolumenVentas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PymeTamFinal.Modelos.ModelosAuxiliares
+{
+    public class GraficaVolumenVentas : IGrafica
+    {
+        public series series { get; set; }
+    }
+    public class series
+    {
+        public string name { get; set; }
+        public double[][] data { get; set; }
+    }
+    public class tooltip
+    {
+        public int valueDecimals { get; set; }
+    }
+    
+}
